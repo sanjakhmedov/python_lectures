d3efecadc8f8c4184221502a065d03e3e9ae332f
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-09-27 03:09:18 +0500

    task 3 is done

--- lec_1/hello.py
+++ lec_1/hello.py
@@ -1,7 +1,21 @@
-print('hello world')
-value = None
-a = 123
-b = 1.23
-print(a, b)
-print(type(value))
-value = 1234
+# print('hello world')
+# value = None
+# a = 123
+# b = 1.23
+# print(a, b)
+# print(type(value))
+# value = 1234
+# a = 'this is string'
+# arr = [3, 2, 3, 2, 3]
+# print(arr)
+#
+# name = input('What is your name? \n')
+# age = int(input('Your age? \n'))
+# print(name)
+# print(age)
+# print(f'My name is {name} and I am {age}')
+
+a = 3
+b = 4
+print(9 // 4) # целая часть от деления
+
