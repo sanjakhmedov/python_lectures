f2c0f712fc4b261be617bc6fdd959990e12b2975
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-10-05 23:00:28 +0500

    lec 2

---
+++ lec_2/hello.py
@@ -0,0 +1,9 @@
+def f(x):
+    if x == 1:
+        return 'Integer'
+    elif x == 2.3:
+        return 23
+    else:
+        return
+
+
---
+++ lec_2/scratch_1.py
@@ -0,0 +1,24 @@
+# colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'a')
+# data.writelines(colors)  # no spaces
+# data.write('\nLine 12\n')  # no spaces
+# data.write('Line 13\n')  # no spaces
+# data.close()
+
+
+# with open('file.txt', 'w') as data:
+#     data.writelines('Line 1\n')
+#     data.writelines('Line 2\n')
+
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+#
+# exit()
+
+import hello as h
+
+print(h.f(1))
